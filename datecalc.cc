5afd40fe5bfd05d189612c847de205dad00c8d00
DjameyH <112989867+DjameyH@users.noreply.github.com>
2022-09-17 12:52:29 +0200

    Add files via upload

--- datecalc.cc
+++ datecalc.cc
@@ -6,6 +6,12 @@ int main()
     int birthyear;
     cout << "enter your birthyear ";
     cin >> birthyear;
+    int month;
+    cout << "enter your birth month ";
+    cin >> month;
+    int birthdate;
+    cout << "enter your birthdate";
+    cin >> birthdate;
     if (birthyear > 2022) //hier komen alle jaren die niet mee doen
     {
        cout << "go back to the future";
@@ -36,10 +42,7 @@ int main()
         cout << "ouder dan honderd en kan nog typen geweldig! maar doei";
         return 1;
     }
-    int month;
-    cout << "enter your birth month "; //en hier alle maanden die niet mee doen
-    cin >> month;
-    if (month == 69)
+    if (month == 69)//en hier alle maanden die niet mee doen
     {
         cout << "nice ;) ";
     }
@@ -53,7 +56,33 @@ int main()
         cout << "dat bestaat nog minder";
         return 1;
     }
-
+    int yearc = 2022;
+    cout << "dit is ongeveer you leeftijd in jaren ";
+    if (month > 9)
+    {
+    cout << yearc - birthyear -1;
+    return 0;
+    }
+    if (month < 9)
+    {
+        cout << yearc - birthyear;
+    }
+    if (month == 9)
+    {
+        if (birthdate < 17)
+        {
+            cout << yearc - birthyear -1;
+        }
+        if (birthyear >= 17)
+        {
+            cout << yearc - birthyear;
+        }
+        if (birthyear == 17)
+        {
+            cout << "Happy birthday!";
+        }
+    }
+    return 0;
 
 
 }
